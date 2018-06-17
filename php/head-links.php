d3964f02595ab9e9e7e54fc5ed994e5fced03fbd
Aubrey Cook <aubreyjcook@gmail.com>
2018-06-17 09:13:18 -0500

    nav ul margin removed

--- php/head-links.php
+++ php/head-links.php
@@ -1,9 +1,10 @@
 <link rel="manifest" href="site.webmanifest">
 <link rel="icon" type="image/png" href="img/favicon.png">
 <?php
+  //loadCSSFile('reset');
   loadCSSFile('normalize');
   loadCSSFile('html5Boilerplate');
-  /*loadCSSFile('grid');*/
+  loadCSSFile('grid');
   loadCSSFile('flexbox');
   loadCSSFile('main');
 ?>
