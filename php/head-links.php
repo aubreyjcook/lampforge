0dff20b2aabef43dc483479815967d7af164ad4c
Aubrey Cook <aubreyjcook@gmail.com>
2018-06-19 13:47:09 -0500

    FlexboxGrid framework implemented via CDN

--- php/head-links.php
+++ php/head-links.php
@@ -1,10 +1,14 @@
 <link rel="manifest" href="site.webmanifest">
 <link rel="icon" type="image/png" href="img/favicon.png">
+<!-- CSS CDNs -->
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" crossorigin="anonymous"> -->
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" crossorigin="anonymous"> -->
+<!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"> -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" >
+<?php // TODO: Update conditionalPageLoading code to load CDNs and implement here ?>
+
 <?php
-  //loadCSSFile('reset');
-  loadCSSFile('normalize');
-  loadCSSFile('html5Boilerplate');
-  loadCSSFile('grid');
-  loadCSSFile('flexbox');
+  // loadCSSFile('grid');
+  // loadCSSFile('flexbox');
   loadCSSFile('main');
 ?>
