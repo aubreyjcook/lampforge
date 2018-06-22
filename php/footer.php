6a710a0d5e4d1669089ed8c2b6a238ca26e60d3d
Aubrey Cook <aubreyjcook@gmail.com>
2018-06-22 10:04:36 -0500

    php modules update

---
+++ php/footer.php
@@ -0,0 +1,11 @@
+<footer class="grid-item row">
+  <section class="grid-item col-lg-4 col-md-4 col-sm-4 col-xs-4">
+    <h1>Footer Section 1</h1>
+  </section>
+  <section class="grid-item col-lg-4 col-md-4 col-sm-4 col-xs-4">
+    <h1>Footer Section 2</h1>
+  </section>
+  <section class="grid-item col-lg-4 col-md-4 col-sm-4 col-xs-4">
+    <h1>Footer Section 3</h1>
+  </section>
+</footer>
---
+++ php/header.php
@@ -0,0 +1,26 @@
+<header class="grid-item row">
+  <div class="grid-item col-lg-4 col-md-4 col-sm-4 col-xs-4">
+    <figure>
+      <h1>Figure</h1>
+    </figure>
+    <figcaption>
+      Lorem ipsum dolor sit amet.
+    </figcaption>
+  </div>
+  <nav class="grid-item col-lg-8 col-md-8 col-sm-8 col-xs-8">
+    <div class="grid-item" id="nav-header">
+      <h1>Nav Header</h1>
+    </div>
+    <ul class="grid-item" id= "nav-list">
+      <li>
+        <a href="#">Link</a>
+      </li>
+      <li>
+        <a href="#">Link</a>
+      </li>
+      <li>
+        <a href="#">Link</a>
+      </li>
+    </ul>
+  </nav>
+</header>
