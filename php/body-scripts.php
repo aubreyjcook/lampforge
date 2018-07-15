61f76980dd1e294036680b5a6016c32526b8d756
Aubrey Cook <aubreyjcook@gmail.com>
2018-07-15 04:26:35 -0500

    body-scripts js files loaded via function

--- php/body-scripts.php
+++ php/body-scripts.php
@@ -1,8 +1,16 @@
-<script src="js/vendor/modernizr-3.5.0.min.js"></script>
+<?php
+  loadJSCDN('https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js', 'sha256-0rguYS0qgS6L4qVzANq4kjxPLtvnp5nn2nB5G1lWRv4=');
+  loadJSCDN('https://code.jquery.com/jquery-3.2.1.min.js', 'sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=');
+
+  loadJSFile('plugins.js');
+  loadJSfile('main.js');
+?>
+
+<!-- <script src="js/vendor/modernizr-3.5.0.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
 <script src="js/plugins.js"></script>
-<script src="js/main.js"></script>
+<script src="js/main.js"></script> -->
 
 <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
 <script>
